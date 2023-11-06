ba6356063187ef57fee6011bbc7f6fab103cf124
Mike Hennessy <127970154+mike-prime@users.noreply.github.com>
2023-11-06 10:32:33 -0500

    fix: Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "iam_role_arn" {
+  description = "The ARN of the IAM role"
+  value       = aws_iam_role.main.arn
+}
+
+output "project_arn" {
+  description = "The ARN of the CodeBuild project"
+  value       = aws_codebuild_project.main.arn
+}
+
 output "image_base_url" {
   description = "The base URL for your ECR images from Docker Hub"
   value       = "${local.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.namespace}/"
